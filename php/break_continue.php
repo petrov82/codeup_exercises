4cc14733e0be5ead500ef09879accb44925665f4
Peter Rhodes <petrov82@aim.com>
2014-02-10 16:07:02 -0600

    notes to prompt adding code for primes

--- php/break_continue.php
+++ php/break_continue.php
@@ -1,18 +1,21 @@
 <?php
 
 // Show the numbers between 1 & 100 using 'continue'
-//
-//
-// Create another 'for' loop that counts from 1 to 100 but stops after 10 using 'break'
-
 for ($number = 1; $number <= 100 ; $number++) { 
 	echo $number . "\n";
 	if ($number % 2 != 0 ) {
 		continue;
 	}
 	echo "^ This is an even number\n";
-}
 
+// find primes by %
+// 	if ($) {
+// 		# code...
+// 	}
+// }
+
+
+// Create another 'for' loop that counts from 1 to 100 but stops after 10 using 'break'
 for ($number = 1; $number <= 100 ; $number++) { 
 	echo $number . "\n";
 	if ($number >= 10 ) {
