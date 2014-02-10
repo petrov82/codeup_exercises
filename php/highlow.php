0f402b93b965eba283138057bfcc0cb9a04d533b
Peter Rhodes <petrov82@aim.com>
2014-02-10 09:43:30 -0600

    high low updates

--- php/highlow.php
+++ php/highlow.php
@@ -1,52 +1,81 @@
 <?php
 
+//dump arg count
+//var_dump($argc);
+
+//dump arg variables
+//var_dump($argv);
+
+//number of arguments must be 3
+//arg 2 & 3 must be numbers
+//arg 2 must be  less than arg 3
+$error = <<<TEST
+Welcome to High/Low!
+To play, type "php highlow.php"
+and then include your minimum and maximum numbers 
+for your guessing range. 
+Then, press the "enter/return" key.
+TEST;
+
+if (($argc != 3) || !is_numeric($argv[1]) || !is_numeric($argv[2]) || ($argv[1] > $argv[2])) {
+	echo $error;
+
+	exit(0);
+
+//When all inputs are correct, run the program. :)	
+} else {
+
+	$minimum = $argv[1];
+	$maximum = $argv[2];
+
 // The specs for the game are:
-do {
-	// - game picks a random number between 1 and 100.
-	$right_number = mt_rand(1, 100);
-
-	$guess_count = 0;
-
-	// - prompts user to guess the number
 	do {
-		fwrite(STDOUT, 'Guess? ');
+		// - game picks a random number between 1 and 100.
+		$right_number = mt_rand($minimum, $maximum);
 
-		$guess = fgets(STDIN);
+		//counts the number of guesses
+		$guess_count = 0;
 
-		// - if user's guess is less than the number, it outputs "HIGHER"
-		// - if user's guess is more than the number, it outputs "LOWER"
-		// - if a user guesses the number, the game should declare "GOOD GUESS!"
-		// - if a user guesses greater than 100 or less than 1, it declares " YOU LOSE"
-		if ($guess < $right_number) {
-			echo "HIGHER\n";
-			$guess_count++;
-		} elseif ($guess > $right_number) {
-			echo "LOWER\n";
-			$guess_count++;
-		}
-	} while ($guess != $right_number);
+		// - prompts user to guess the number
+		do {
+			fwrite(STDOUT, 'Make a guess: ');
+
+			$guess = fgets(STDIN);
+
+			// - if user's guess is less than the number, it outputs "HIGHER"
+			// - if user's guess is more than the number, it outputs "LOWER"
+			// - if a user guesses the number, the game should declare "GOOD GUESS!"
+			// - if a user guesses greater than 100 or less than 1, it declares " YOU LOSE"?
+			if ($guess < $right_number) {
+				echo "Try a HIGHER number!\n";
+				$guess_count++;
+			} elseif ($guess > $right_number) {
+				echo "Try a LOWER number¡\n";
+				$guess_count++;
+			}
+		} while ($guess != $right_number);
 
 
-	echo "GOOD GUESS\n";
-	echo "$guess_count\n";
+		echo "GOOD GAME!\n";
+		echo "You made $guess_count guess(es).\n";
 
-	fwrite(STDOUT, "PLAY AGAIN Y/N?");
-	$again = fgets(STDIN);
+		fwrite(STDOUT, "WOULD YOU LIKE TO PLAY AGAIN, Y/N?");
+		$again = fgets(STDIN);
 
-} while ($again == 'Y');
+	// - if a user wins or loses, the game asks if the user wants to play again, asks Y/N
+	} while ($again == 'Y' || "y" || "YES" || "Yes" || "yes");
 
-$end = ["gg bro gg\n", "the only way to win is not to play\n"];
-	echo $end[mt_rand(0,1)];
-//}
- 
-// - if a user wins or loses, the game asks if the user wants to play again, asks Y/N
-// - if Y, loop back to begin game, if N, rand('gg bro gg' , 'the only way to win is not to play)'
+	// - if Y, loop back to begin game, if N, rand('gg bro gg' , 'the only way to win is not to play)
+	$end = ["gg bro gg\n", "the only way to win is not to play\n"];
+		echo $end[mt_rand(0,1)];
+}
 
-// Hints:
-// - Using conditionals and loops here is important
-// - Random numbers can be made with the internal rand() function
-// - If user is right, tell them they won
-// - While they are wrong, give them hints and keep asking
-// - Use exit(0) to end the game
-// - If you get stuck in game, ctrl-c will exit.
-?>
+
+	// Hints:
+	// - Using conditionals and loops here is important
+	// - Random numbers can be made with the internal rand() function
+	// - If user is right, tell them they won
+	// - While they are wrong, give them hints and keep asking
+	// - Use exit(0) to end the game
+	// - If you get stuck in game, ctrl-c will exit.
+	?>
