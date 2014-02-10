2a2af0634a7e53e99e54b6f7b4c067dec346aec7
Peter Rhodes <petrov82@aim.com>
2014-02-10 11:24:48 -0600

    step 2 foreach done

--- php/foreach.php
+++ php/foreach.php
@@ -2,12 +2,32 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 foreach ($things as $thing) {
+
+	if (is_bool($thing)) {
+		echo "$thing is a bool\n";
+	}
+
+	if (is_int($thing)) {
+		echo "$thing is an integer\n";
+	}
+
+	if (is_float($thing)) {
+		echo "$thing is a double\n";
+	}
+
+	if (is_null($thing)) {
+		echo "$thing is null\n";
+	}
 	
 	if (is_array($thing)) {
-		echo "$thing is an array\n";
-	}
-		else {
-			echo "$thing is not an array\n";
+		foreach ($thing as $number) {
+			echo "$number is a number\n";
 		}
+		echo "\$thing is an array\n";
+	}
+
+	if (is_string($thing)) {
+		echo "$thing is a string\n";
+	}
 }
 ?>
