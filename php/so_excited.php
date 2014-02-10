d30bc273835f2cdbc6bf5ae3b1fff5d46a6adf67
Peter Rhodes <petrov82@aim.com>
2014-02-10 13:57:18 -0600

    copy of high low code for new game

--- php/so_excited.php
+++ php/so_excited.php
@@ -1,8 +1,20 @@
 <?php
 
+
+// Write the output
+// Notice the space after the ?
+fwrite(STDOUT, 'Guess? ');
+
+// Get the input from user
+$answer = fgets(STDIN);
+
+// Output the user's name
+fwrite(STDOUT, "Hello $answer\n");
+
 $a = 'so excited';
 $b = 'can\'t hide it';
 $c = 'about to lose control';
 $d = 'like it';
+$z = 'so scared';
 
 ?>
