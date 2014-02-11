8db7f7d01feacd53a7691d490202e1935ada4ed3
Peter Rhodes <petrov82@aim.com>
2014-02-11 13:22:42 -0600

    add arithmetic base file

--- php/arithmatic.php
+++ php/arithmatic.php
@@ -1,5 +1,20 @@
 <?php
 
+// intval($a [$base = 0]);
+// intval($b [$base = 0]);
+
+
+// Commit your changes for each step and push to GitHub.
+
+// Validate all the arguments, and display an error if the input is not numeric.
+
+// Validate divide by 0 errors, display error if attempts to divide by 0 are made.
+
+// Make the error messages show the values of the arguments.
+
+// Refactor the error messages into their own function, have the other functions use it for error messaging.
+
+
 function add($a, $b) {
 	echo "$a + $b = "; 
 	echo $a + $b . PHP_EOL;
