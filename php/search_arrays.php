26554aeb5fe8364d2f887a6e793ad36f561e40c8
Peter Rhodes <petrov82@aim.com>
2014-02-12 16:40:26 -0600

    step 2 $names/$premiumusers search

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -1,13 +1,28 @@
 <?php
 
 //first names
-$registered_users = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+$premium_users = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $currently_logged_in = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
+function IsInArray($array, $value) {
+	return in_array($value, $array);
+}
 
+function compareArray($array1, $array2) {
 
+}
 
+$query = 'Mike';
+
+$result = array_search($query, $premium_users);
+
+if ($result) {
+    echo "$premium_users[$result] is TRUE";
+
+	} else {
+    		echo "$query is FALSE";
+    	}
 
 
 ?>
