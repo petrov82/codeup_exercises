5835c2077dcf370678582d5079917914b5a6bc71
Peter Rhodes <petrov82@aim.com>
2014-02-11 23:12:48 -0600

    check $something

--- php/internal-functions.php
+++ php/internal-functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$nothing = NULL;
+$nothing = 0xFF;
 $something = '';
 $array = array(1,2,3);
 
@@ -14,32 +14,51 @@ function set_or_empty($checkvar) {
 		return TRUE;
 	}
 }
-
-if (set_or_empty($nothing)) {
-
-	echo "\$nothing is SET\n";
-} else {
-
-	echo "\$nothing is not a SET\n";
-}
-
-if (set_or_empty($nothing)) {
-
-	echo "\$nothing is not EMPTY\n";
-
-} else {
-	echo "\$nothing is EMPTY\n";
-}
-
 // TEST: If var $nothing is set, display '$nothing is SET'
-
-// TEST: If var $nothing is empty, display '$nothing is EMPTY'
-
 // TEST: If var $something is set, display '$something is SET'
+if (set_or_empty($something)) {
+
+	echo "\$something ({$something}) is a SET\n";
+
+} else {
+
+	echo "\$something ({$something}) is not a SET\n";
+}
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+// TEST: If var $something is empty, display '$something is EMPTY'
+if (set_or_empty($something)) {
+
+	echo "\$something ({$something}) is not EMPTY\n";
+
+} else {
+	echo "\$something ({$something}) is EMPTY\n";
+}
 
 // Serialize the array $array, and output the results
+// $array = array(1,2,3,4,5,6,7,8,9,0);
 
+$serialized = serialize($array);
+
+echo $serialized  . PHP_EOL;
 // Unserialize the array $array, and output the results
+$deser = unserialize($serialized);
+
+var_dump($deser);
+print_r($deser);
+foreach ($deser as $key => $value) {
+	
+	if (is_array($thing)) {
+		foreach ($thing as $number) {
+			echo "$number\n";
+		}
+	}
+
+		
+	else {
+			echo "$thing\n";
+			}
+}
+
 
 
 
---
+++ php/zinternal-functions.php
@@ -0,0 +1,26 @@
+<?php
+
+$nothing = NULL;
+$something = '';
+$array = array(1,2,3);
+
+echo (isset($something));
+
+
+
+
+// $array = array(1,2,3,4,5,6,7,8,9,0);
+
+// $serialized = serialize($array);
+
+// echo $serialized  . PHP_EOL;
+
+// $deser = unserialize($serialized);
+
+// echo $deser . PHP_EOL;
+
+// exit(0);
+
+
+
+?>
