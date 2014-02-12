3506c5185987a956dde13a41ec1d68e90d5effd0
Peter Rhodes <petrov82@aim.com>
2014-02-12 17:29:42 -0600

    search for bob, print array difference

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -1,28 +1,36 @@
 <?php
 
-//first names
+// Array of names in a Premium user array
 $premium_users = ['Tina', 'Dana', 'Amy', 'Mike', 'Adam'];
-
+// Array of names in current-logged-in file
 $currently_logged_in = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
+
+// Function to search within an array
 function IsInArray($array, $value) {
+
 	return in_array($value, $array);
 }
 
-function compareArray($array1, $array2) {
 
+// Function compares differences between 2 arrays, Array 2 is base, array 1 yeilds names not in base array
+function compareArray($array1, $array2) {
+	 
+	 return array_diff($array1, $array2);
 }
+
 
 $query = 'BOB';
 
 $result = array_search($query, $premium_users);
 
 if ($result) {
-    echo "$premium_users[$result] is a premium user";
+    echo "$premium_users[$result] is a premium user" . PHP_EOL;
 
 	} else {
-    		echo "$query is FALSE";
+    		echo "$query is not a premium user" . PHP_EOL;
     	}
 
+print_r(array_diff($premium_users, $currently_logged_in))
 
 ?>
