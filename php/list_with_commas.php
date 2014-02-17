4ab53915fb52a88bffdd57a640af67c5b7e74f1b
Peter Rhodes <petrov82@aim.com>
2014-02-17 08:55:20 -0600

    create a serial list in a string that includes "and"

--- php/list_with_commas.php
+++ php/list_with_commas.php
@@ -12,20 +12,36 @@ $physicists_array = explode(', ', $physicists_string);
 
 	// var_dump($physicists_array);
 
+// function alpha_list($alpha = FALSE) {
+
+// 	$list = $physicists_array;
+
+// 	if ($alpha) {
+
+// 		return sort($list);
+// 	}
+// }
+
+
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($array) {
+function humanized_list($array, $sort = FALSE) {
+
+	if ($sort) {
+		# code...
+	}
 
 	$last_item = array_pop($array);
 
 	$other_names = implode(', ', $array);
 
-	return "$other_names, and $last_item";
+	return "$other_names, and $last_item" . PHP_EOL;
 
-}
+	}
 
 
 
-echo (humanized_list($physicists_array));
+
+echo (humanized_list($physicists_array, $alpha = TRUE));
 
 // var_dump($physicists_array);
 
